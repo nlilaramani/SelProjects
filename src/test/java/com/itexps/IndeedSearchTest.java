682ae5fcf6bc67d83d8df387a9ba5f39a1a44dc1
unknown <nlilaramnai@hotmail.com>
2018-09-29 08:58:03 -0500

    sel prj

---
+++ src/test/java/com/itexps/IndeedSearchTest.java
@@ -0,0 +1,81 @@
+package com.itexps;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class IndeedSearchTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "c:\\qa\\drivers\\chromedriver.exe");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.indeed.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testIndeedSearch() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.id("text-input-what")).clear();
+    driver.findElement(By.id("text-input-what")).sendKeys("java");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    try {
+      assertEquals("Java Jobs, Employment in Palatine, IL | Indeed.com", driver.getTitle());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
